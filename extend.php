0fcdf3272c7d9a52783a0a987599e71323a0264b
snfansclub <67875881+snfansclub@users.noreply.github.com>
2020-07-06 20:50:50 +0700

    Update extend.php

--- extend.php
+++ extend.php
@@ -7,15 +7,19 @@ use s9e\TextFormatter\Configurator;
 return [
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
-		$config->MediaEmbed->add(
-				'vlive',
-				[
-					'host'	  => 'vlive.tv',
-					'extract' => "!vlive\.tv/video/(?'ClipId'[-0-9]+)!",
-					'iframe' => [
-						'src'  => '//www.vlive.tv/embed/{@ClipId}'
-					]
-				]
-			);
+             $config->MediaEmbed->add(
+    'vlive',
+    [
+        'host'    => ['vlive.tv'],
+        'extract' => [
+            "!vlive\\.tv/video\\(?'ClipId'[-0-9A-Z_a-z]+)!",
+        ],
+        'iframe'  => [
+            'width'  => 560,
+            'height' => 315,
+            'src'    => 'http://www.youtube.com/embed/{@ClipId}}'
+        ]
+    ]
+);
 	   })
 ];
